8f42b88d7acd262f6c121552766ac18568fd4fd5
crisglezcal <glezcalcris95@gmail.com>
2025-09-29 16:02:22 +0200

    add/ej10.1

--- scripts.js
+++ scripts.js
@@ -44,23 +44,37 @@ function imprimePregunta(pregunta) {
 }
 
 function imprimeTitulo(pregunta) {
-  // Put your code here
+  function imprimeTitulo(pregunta) {
+  return "<p>" + pregunta.titulo + "</p>";
+}
 }
 
 function imprimeTodasLasRespuestas(pregunta) {
-  // Put your code here
+  function imprimeTodasLasRespuestas(pregunta) {
+  let respuestaImprimir = "";
+  for (let i = 0; i < pregunta.respuestas.length; i++){
+    respuestasImprimir =+ imprimeUnaRespuesta(i);
+  }
+  return respuestaImprimir;
+}
 }
 
 function imprimeUnaRespuesta(respuesta) {
-  // Put your code here
+  function imprimeUnaRespuesta(respuesta) {
+  return imprimeLabel(respuesta) + imprimeInput (respuesta);
+}
 }
 
 function imprimeLabel(respuesta) {
-  // Put your code here
+  function imprimeLabel(respuesta) {
+  return `<label for=${pregunta.respuestas[respuesta].id}>${pregunta.respuestas[respuesta].label}</label>`;
+}
 }
 
 function imprimeInput(respuesta) {
-  // Put your code here
+  function imprimeInput(respuesta) {
+  return `<input id=${pregunta.respuestas[respuesta].id} name=${pregunta.respuestas[respuesta].name} type="radio" value=${pregunta.respuestas[respuesta].value}>`;
+}
 }
 
 document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
